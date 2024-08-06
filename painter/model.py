15e0041b09c7ea33d670198ddb417f1c9959e456
Mario <luismariovasquezcorrea@gmail.com>
2024-08-06 13:32:40 -0500

    Class Rectangle

--- painter/model.py
+++ painter/model.py
@@ -44,3 +44,20 @@ class Triangle:
     def __str__(self):
         return "Triangle with vertices at ", (self.point_1.x, self.point_1.y1), (self.point_2.x, self.point_2.y), " and ", (self.point_3.x, self.point_3.y)
 
+class Rectangle:
+    def __init__(self, point_1, point_2) -> Point(x, y):
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+    def area(self) -> float:
+        return point_1 * point_2
+
+    def draw(self):
+        x = [self.point_1.x, self.point_2.x, self.point_2.x, self.point_1.x, self.point_1.x]
+        y = [self.point_1.y, self.point_1.y, self.point_2.y, self.point_2.y, self.point_1.y]
+        plt.fill(x, y, color='g')
+        plt.axis("scaled")
+        plt.show()
+
+    def __str__(self):
+        return "Rectangle with vertices at ", (self.point_1.x, self.point_1.y), " and ", (self.point_2.x, self.point_2.y)
